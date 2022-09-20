de347ec3aea960cb4c24f9dbde3cd0d46cf1b802
carlos815 <carlos_t815@outlook.com>
2022-09-20 11:40:27 -0500

    Clean up chips array

--- components/ChipsArray.tsx
+++ components/ChipsArray.tsx
@@ -1,15 +1,8 @@
-
-
-
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
 import Chip from '@mui/material/Chip';
-import TagFacesIcon from '@mui/icons-material/TagFaces';
-import { Group, Item } from '../types/types';
-import { deleteGroup, updateGroup } from '../api/fetch';
+import { Item } from '../types/types';
 import Paper from '@mui/material/Paper';
 import DoneIcon from '@mui/icons-material/Close';
-
 
 export default function ChipsArray(props: { items: Item[] | undefined, handleToggle?: Function, simple?: boolean }) {
     return (
@@ -37,18 +30,14 @@ export default function ChipsArray(props: { items: Item[] | undefined, handleTog
                             // color={data.active ? "primary" : "default"}
                             variant={data.active ? "filled" : "outlined"}
                             size="small"
-                        />
-
-
-
-                            : <Chip
-                                icon={icon}
-                                label={data.name}
-                                color={data.active ? "primary" : "default"}
-                                variant={data.active ? "filled" : "outlined"}
-                                onDelete={() => { props.handleToggle && props.handleToggle(data) }}
-                                deleteIcon={data.active ? <DoneIcon /> : <DoneIcon style={{ display: "none" }} />}
-                                onClick={() => { props.handleToggle && props.handleToggle(data) }} />}
+                        /> : <Chip
+                            icon={icon}
+                            label={data.name}
+                            color={data.active ? "primary" : "default"}
+                            variant={data.active ? "filled" : "outlined"}
+                            onDelete={() => { props.handleToggle && props.handleToggle(data) }}
+                            deleteIcon={data.active ? <DoneIcon /> : <DoneIcon style={{ display: "none" }} />}
+                            onClick={() => { props.handleToggle && props.handleToggle(data) }} />}
                     </div>
 
                 );
